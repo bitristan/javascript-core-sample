8b68a5fb8b6bb8b4fad4c6b4d29e1ddaddb5b814
ts <sunting.bcwl@gmail.com>
2022-03-08 08:19:55 +0800

    第一个可用版本。

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,37 @@
 #include <jni.h>
 #include <string>
 
+#include "JavaScriptCore/JavaScript.h"
+
+std::string JSStringToStdString(JSStringRef jsString) {
+    size_t maxBufferSize = JSStringGetMaximumUTF8CStringSize(jsString);
+    char* utf8Buffer = new char[maxBufferSize];
+    size_t bytesWritten = JSStringGetUTF8CString(jsString, utf8Buffer, maxBufferSize);
+    std::string utf_string = std::string(utf8Buffer, bytesWritten - 1);
+    delete [] utf8Buffer;
+    return utf_string;
+}
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_javascript_engine_sample_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    JSContextGroupRef contextGroup = JSContextGroupCreate();
+    JSGlobalContextRef globalContext = JSGlobalContextCreateInGroup(contextGroup, nullptr);
+
+    JSStringRef statement = JSStringCreateWithUTF8CString(
+            "function degToRad(value) {return (value * Math.PI) / 180; } ('90 deg = ' + degToRad(90))");
+
+    JSValueRef retValue = JSEvaluateScript(globalContext, statement, nullptr, nullptr, 1, nullptr);
+
+    JSStringRef retString = JSValueToStringCopy(globalContext, retValue, nullptr);
+
+    std::string hello = JSStringToStdString(retString);
+
+    JSGlobalContextRelease(globalContext);
+    JSContextGroupRelease(contextGroup);
+    JSStringRelease(statement);
+    JSStringRelease(retString);
+
     return env->NewStringUTF(hello.c_str());
 }
